4116b03793d5f88bec8ee52347f55e0aea594f9a
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-09-03 18:47:33 -0400

    confirmed get and post fetch work with api

--- src/App.js
+++ src/App.js
@@ -3,13 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/users', {method:'GET', headers: {Authorization: `Bearer<token>`}})
+    .then(resp=>resp.json())
+    .then(data=>console.log(data))
+  }
+
+  newUserPost() {
+    fetch('http://localhost:3000/api/v1/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        user: {
+          username: 'mimi1',
+          password: 'itme',
+          name: 'Mimi'
+        }
+      })
+    })
+    .then(resp => resp.json())
+    .then(console.log)
+  }
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <button onClick={this.newUserPost}>Add User</button>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
