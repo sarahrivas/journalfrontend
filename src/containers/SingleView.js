924eb6b03a7dbd9054ce7b122d140af9da609a5a
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-09-11 14:52:38 -0400

    card structure for recipe added

--- src/containers/SingleView.js
+++ src/containers/SingleView.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { editEntry, resetEntry, deleteEntry,renderSingleEntry } from '../actions';
 import { withRouter } from 'react-router';
@@ -52,7 +52,11 @@ const styles = theme => ({
 class SingleView extends Component {
 
   state = { expanded: false };
-  
+
+  handleExpandClick = () => {
+    this.setState(state => ({ expanded: !state.expanded }));
+  };
+
   deleteHandler = (event) => {
     console.log(this);
     const id = this.props.currentEntry.id;
@@ -81,12 +85,82 @@ class SingleView extends Component {
   render() {
 
     return (
-      <div className="single-entry">
-        {this.props.currentEntry.title}
+      <Fragment>
+      <Card>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Recipe">
+              <img src="https://img.clipartxtras.com/e1f272d5006b508abb031b710400ed6a_chef-logo-clip-art-royalty-free-gograph-chef-hat-clipart-free_170-169.jpeg"/>
+            </Avatar>
+          }
+          action={
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title={this.props.currentEntry.title}
+          subheader={this.props.currentEntry.created_at}
+        />
+        <CardMedia
+          image="/static/images/cards/paella.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography component="p">
+            This impressive paella is a perfect party dish and a fun meal to cook together with your
+            guests. Add 1 cup of frozen peas along with the mussels, if you like.
+          </Typography>
+        </CardContent>
+        <CardActions disableActionSpacing>
+          <IconButton aria-label="Add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="Share">
+            <ShareIcon />
+          </IconButton>
+          <IconButton
+            onClick={this.handleExpandClick}
+            aria-expanded={this.state.expanded}
+            aria-label="Show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph variant="body2">
+              Method:
+            </Typography>
+            <Typography paragraph>
+              Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+              minutes.
+            </Typography>
+            <Typography paragraph>
+              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+              heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+              browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
+              chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
+              salt and pepper, and cook, stirring often until thickened and fragrant, about 10
+              minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+            </Typography>
+            <Typography paragraph>
+              Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+              without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat
+              to medium-low, add reserved shrimp and mussels, tucking them down into the rice, and
+              cook again without stirring, until mussels have opened and rice is just tender, 5 to 7
+              minutes more. (Discard any mussels that don’t open.)
+            </Typography>
+            <Typography>
+              Set aside off of the heat to let rest for 10 minutes, and then serve.
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+
         {this.props.currentEntry.content}
           <button onClick={this.deleteHandler}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/></svg></button>
           <button onClick={this.editHandler}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg></button>
-      </div>
+      </Fragment>
     );
   }
 }
@@ -95,6 +169,6 @@ const mapStateToProps = state => ({
   currentEntry: state.entry.currentEntry
 })
 
-export default withRouter(connect(
+export default withStyles(styles)(withRouter(connect(
   mapStateToProps
-)(SingleView));
+)(SingleView)));
