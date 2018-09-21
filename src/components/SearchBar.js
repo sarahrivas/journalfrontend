ffe6aef79684ae6b50953171698aaa620071379e
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-09-21 18:10:18 -0400

    search bar css

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,9 +5,49 @@ import { withStyles } from '@material-ui/core/styles';
 
 const SearchBar = (props) => {
 
+  const styles = theme => ({
+    searchIcon: {
+      width: theme.spacing.unit * 9,
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+      width: '100%',
+    },
+    inputInput: {
+      paddingTop: theme.spacing.unit,
+      paddingRight: theme.spacing.unit,
+      paddingBottom: theme.spacing.unit,
+      paddingLeft: theme.spacing.unit * 10,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: 120,
+        '&:focus': {
+          width: 200,
+        },
+      },
+    },
+  });
+
   return (
     <Fragment>
-
+    <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+    <Input
+            placeholder="Search…"
+            disableUnderline
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+          />
     </Fragment>
   )
 }
